9c11528f068bc9528b2c33b9407660e2918aefd7
cash <cash@cashwu.com>
2020-08-20 16:42:54 +0800

    MassTransit console test

---
+++ Receiver/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EventContracts;
+using MassTransit;
+
+namespace EventContracts
+{
+    public interface ValueEntered
+    {
+        string Value { get; }
+    }
+}
+
+namespace Receiver
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host("127.0.0.1", "/", h =>
+                {
+                    h.Username("cash");
+                    h.Password("cash");
+                });
+
+                cfg.ReceiveEndpoint("event-listener", e =>
+                {
+                    e.Consumer<EventConsumer>();
+                });
+            });
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await busControl.StartAsync(source.Token);
+
+            try
+            {
+                Console.WriteLine("Press enter to exit");
+
+                await Task.Run(Console.ReadLine, source.Token);
+            }
+            finally
+            {
+                await busControl.StopAsync(source.Token);
+            }
+        }
+    }
+
+    class EventConsumer : IConsumer<ValueEntered>
+    {
+        public Task Consume(ConsumeContext<ValueEntered> context)
+        {
+            Console.WriteLine("Received Value: {0}", context.Message.Value);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using EventContracts;
+using MassTransit;
+
+namespace EventContracts
+{
+    public interface ValueEntered
+    {
+        string Value { get; }
+    }
+}
+
+namespace Sender
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(configurator =>
+            {
+                configurator.Host("127.0.0.1", "/", h =>
+                {
+                    h.Username("cash");
+                    h.Password("cash");
+                });
+            });
+
+            await busControl.StartAsync();
+
+            try
+            {
+                do
+                {
+                    var value = await Task.Run(() =>
+                    {
+                        Console.WriteLine("Enter message (or quit to exit)");
+                        Console.Write("> ");
+
+                        return Console.ReadLine();
+                    });
+
+                    if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+
+                    await busControl.Publish<ValueEntered>(new
+                    {
+                        Value = value
+                    });
+                }
+                while (true);
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
